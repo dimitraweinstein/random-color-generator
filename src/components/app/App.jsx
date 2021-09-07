cc19b0565c760c3a6b2982a662f9dbf3c1543b32
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-07 16:59:35 -0700

    created a containers folder and updated file structure, imports and component names

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import RandomColor from '../RandomColor';
-import Display from '../Display';
+import RandomColor from '../../containers/RandomColor';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <RandomColor />;
 }
---
+++ src/containers/RandomColor.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import setBgColor from '../components/color-generator/SetColor';
+import Display from '../components/color-generator/Display';
+
+class RandomColor extends Component {
+
+}
+
+
+export default RandomColor;
