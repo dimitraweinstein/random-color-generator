e52915afeb6eeaba612fbe92d2a52a9fbbcc17cf
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-08 09:57:41 -0700

    something is rendered in the live serve, but the random color is not generating

--- src/components/color-generator/Display.jsx
+++ src/components/color-generator/Display.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = () => {
-  
+const Display = ({ bgColor }) => {
+  return <div style={{ background: bgColor }}>{bgColor}</div>;
 };
 
+Display.propTypes = {
+  bgColor: PropTypes.string.isRequired
+};
 
 export default Display;
 
--- src/components/color-generator/SetColor.jsx
+++ src/components/color-generator/SetColor.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const setBgColor = ({ bgColor }) => {
-
-  const randomColor = Math.floor(
-    Math.random() * 16777215).toString(16);
+  const randomColor = 
+    Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = '#' + randomColor;
   bgColor.innerHTML = '#ff8800';
-  setInterval(setBgColor, 1000);
+
+  setInterval(() => {
+    bgColor = setBgColor;
+  }, 1000);
 
   setBgColor();
 
   return (
     <>
-      <div />
+      <div style={{ background: bgColor }} />
     </>
-
   );
   
 };
 
 setBgColor.propTypes = {
-  bgColor: PropTypes.string.isRequired
+  bgColor: PropTypes.string.isRequired,
 };
 
 export default setBgColor;
